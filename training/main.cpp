f630d9adab2951bf4a4bc836a80ec3c775901dae
Tamer Halwagy <tmr_halwagy@yahoo.com>
2017-02-21 04:12:37 +0200

    Initial Commit

---
+++ training/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  training
+//
+//  Created by Tamer El Halwagy on 2/21/17.
+//  Copyright © 2017 Tamer El Halwagy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
